5f59fb082b6b6211f5c6f018167fb8219561b038
Dan Saurina <dansaurina@gmail.com>
2020-06-28 10:23:06 -0400

    Modified About.js

--- views/pages/About.js
+++ views/pages/About.js
@@ -1,25 +1,26 @@
 let About = {
     render : async () => {
         let view =  /*html*/`
-            <section class="section" align="center">
+            <div align="center">
                 <h1> AWS Architecture </h1>
                 <a href="https://www.cloudtopia.org/">
                 <img alt="Services" src="/spa/images/aws-resources.png"
                 width=680" height="480">
              </a>
-            </section>
-            <section class="section" align="center">
+            </div>
+            <div align="center">
                 <h1> AWS Services Levels </h1>
                 <a href="https://www.cloudtopia.org/">
                 <img alt="Services" src="/spa/images/services.jpg"
                 width=680" height="480">
-             </a>
-            </section>
+                 </a>
+            </div>
         `
         return view
     },
     after_render: async () => {}
         
 }
+
 
 export default About;
